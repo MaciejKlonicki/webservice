6f2c02fcc48f79768331e869e5f43ed4e165fc76
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-19 15:24:33 +0200

    first commit

---
+++ src/test/java/pl/maciejklonicki/ytapp/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejklonicki.ytapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
