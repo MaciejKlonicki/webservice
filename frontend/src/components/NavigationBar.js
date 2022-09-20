8c9304347690cdeb105e06701a1503ed824284e8
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-20 10:41:54 +0200

    I added icons to my navbar

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import {Container, Nav, Navbar} from 'react-bootstrap';
+import { FaHome } from 'react-icons/fa';
+import { CgProfile } from 'react-icons/cg';
+import { FiSettings } from 'react-icons/fi';
+import { BiLogIn } from 'react-icons/bi';
+import { BsFillPersonPlusFill} from 'react-icons/bs';
 
 class NavigationBar extends React.Component {
   render() {
@@ -9,9 +14,11 @@ class NavigationBar extends React.Component {
           <Container>
             <Navbar.Brand href='/'>Web Service</Navbar.Brand>
             <Nav className='me-auto'>
-              <Nav.Link href='/'>Home</Nav.Link>
-              <Nav.Link href='/profile'>Profile</Nav.Link>
-              <Nav.Link href='/setting'>Settings</Nav.Link>
+              <Nav.Link href='/'><FaHome />{' '}Home</Nav.Link>
+              <Nav.Link href='/profile'><CgProfile />{' '}Profile</Nav.Link>
+              <Nav.Link href='/setting'><FiSettings />{' '}Settings</Nav.Link>
+              <Nav.Link href='/register' style={{position: 'absolute', right: 0}}><BsFillPersonPlusFill />{' '}Register</Nav.Link>
+              <Nav.Link href='/login' style={{position: 'absolute', right: 100}}><BiLogIn />{' '}Login</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
