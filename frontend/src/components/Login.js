3944b758296ced5b21361b8ca88685f31c5ddd5f
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-20 12:04:35 +0200

    I added button to login and to reset the form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Button, Card } from 'react-bootstrap';
 import './Login.css';
+import { FaUndo, FaSignInAlt } from 'react-icons/fa';
 
 class Login extends Component {
+
+    resetLoginForm = () => {
+        this.setState(() => this.initialState);
+    };
+
   render() {
     return (
         <div className='Auth-form-container'>
             <form className='Auth-form'>
                 <div className='Auth-form-content'>
-                    <h3 className='Auth-form-title'>Sign In</h3>
+                    <h3 className='Auth-form-title'>Login</h3>
                     <div className='form-group mt-3'>
                         <label>Email Address</label>
                         <input
@@ -23,9 +30,14 @@ class Login extends Component {
                             placeholder="Enter password" />
                 </div>
                 <div className="d-grid gap-2 mt-3">
-                    <button type="success" className="btn btn-success" style={{width : "20%"}}>
-                        Login
-                    </button>
+                    <Card.Footer style={{"textAlign":"left"}}>
+                    <Button size="sm" type="success" className="btn btn-success" style={{width : "25%"}}>
+                        <FaSignInAlt />{' '}Login
+                    </Button>{' '}
+                    <Button size="sm" type="info" className="btn btn-info" style={{width : "25%"}} onClick={this.resetLoginForm}>
+                        <FaUndo />{' '}Reset
+                    </Button>
+                    </Card.Footer>
                 </div>
                 </div>
             </form>
