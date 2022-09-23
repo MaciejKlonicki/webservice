4643c66473a328e978cdf5731b8679868971047d
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-09-23 16:59:23 +0200

    Settings page is on works

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import { Card, Nav } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import { ListGroup } from 'react-bootstrap';
 import './Settings.css';
 
 class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      change: '',
+    }
+    this.handel = this.handel.bind(this);
+  }
+
+  handel = (element) => {
+    element = <Card bg="dark" variant="dark" style={{position: 'relative', left: '335px',bottom: '335px', width: '500px', height: '500px', borderRadius: '0px', borderRadius: "5px"}}>
+      <Card.Body style={{color: "white"}}>
+        <Card.Title>Language</Card.Title>
+        <br></br>Wybierz język:
+      </Card.Body>
+    </Card>
+    this.setState({
+      change: element
+    });
+  }
+
   render() {
     return (
       <Card bg="dark" variant="dark" style={{ width: '250px', height: '93.7vh', borderRadius: "0px"}}>
       <Card.Body>
         <Card.Title style={{color: "white", textTransform: "uppercase", fontFamily: "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif", fontWeight: "500", letterSpacing: "10px", textAlign: "center", borderBottom: "1px solid rgba(255,255,255,.25)"}}>general settings</Card.Title>
         <ListGroup>
-        <Nav.Link style={{background: "transparent", color: "gray", borderRadius: "0px", textAlign: "center"}} href="/language" className="list-group-item"><p style={{position: "relative", marginBottom: "2px", fontFamily:"Arial, Helvetica, sans-serif"}}>Language</p></Nav.Link>
-        <Nav.Link style={{background: "transparent", color: "gray", borderRadius: "0px", textAlign: "center"}} href="/about" className="list-group-item"><p style={{position: "relative", marginBottom: "2px", fontFamily:"Arial, Helvetica, sans-serif"}}>About</p></Nav.Link>
+        <button onClick={this.handel} style={{background: "transparent", color: "gray", borderRadius: "0px", textAlign: "center"}} className="list-group-item"><p style={{position: "relative", marginBottom: "2px", fontFamily:"Arial, Helvetica, sans-serif"}}>Language</p></button>
+        <button style={{background: "transparent", color: "gray", borderRadius: "0px", textAlign: "center"}} className="list-group-item"><p style={{position: "relative", marginBottom: "2px", fontFamily:"Arial, Helvetica, sans-serif"}}>About</p></button>
         </ListGroup>
       </Card.Body>
+      {this.state.change}
     </Card>
     )
   }
