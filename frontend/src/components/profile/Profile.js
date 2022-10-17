664c4196fc0bdb21cb874203b5ad6a0c4bc8a20b
MaciejKlonicki <maciek.klonicki@gmail.com>
2022-10-17 15:09:46 +0200

    I used axios to get my data from backend to front

--- frontend/src/components/profile/Profile.js
+++ frontend/src/components/profile/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './Profile.css';
+import UserService from './UserService';
 
-const Profile = () => {
+class Profile extends React.Component {
+    constructor(props) {
+        super(props)
 
-  return (
+        this.state = {
+            users: []
+        }
+    }
+
+    componentDidMount() {
+        UserService.getUsers().then((res) => {
+            this.setState({users: res.data});
+        })
+    }
+
+    render() {
+    return (
     <div>
         <div className='profile_img text-center p-4'>
             <div className='flex flex-column justify-content-center align-items-center'>
@@ -24,11 +39,35 @@ const Profile = () => {
                             top: "50px",
                             right: "740px",
                             color: 'white'
-                        }}>{localStorage.getItem("email")}</p>
+                        }}></p>
             </div>
+        </div>
+        <div className='row'>
+        <table className='table table-striped table-bordered'>
+            <thead>
+                <tr>
+                    <th>username</th>
+                    <th>email</th>
+                    <th>mobile</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    this.state.users.map(
+                        user => 
+                        <tr key={user.id}>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{user.mobile}</td>
+                        </tr>
+                    )
+                }
+            </tbody>
+        </table>
         </div>
     </div>
   )
+}
 }
 
 export default Profile
---
+++ frontend/src/components/profile/UserService.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const USER_API_URL = "http://localhost:8080/api/users";
+
+class UserService {
+    getUsers() {
+        return axios.get(USER_API_URL);
+    }
+}
+
+export default new UserService();
